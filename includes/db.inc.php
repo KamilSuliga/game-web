2b63eaa090cf63ad11d0ec353d97c92f7de9e629
KamilSuliga <80487619+KamilSuliga@users.noreply.github.com>
2023-02-01 02:41:48 +0100

    Add files via upload

---
+++ includes/db.inc.php
@@ -0,0 +1,10 @@
+<?php
+$serverName="localhost";
+$dbUserName="root";
+$dbPassword="";
+$dbName="project";
+$conn=mysqli_connect($serverName,$dbUserName,$dbPassword,$dbName);
+if(!$conn){
+    die("Connection Failed: ".mysqli_connect_error());
+}
+?>
